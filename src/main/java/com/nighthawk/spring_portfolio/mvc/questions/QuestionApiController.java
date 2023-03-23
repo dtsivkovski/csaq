0e026e7f1cfebd5b207b8e990190f8ff3e0b0318
YashShah138 <89223735+YashShah138@users.noreply.github.com>
2023-03-23 08:41:49 -0700

    mcq stuff

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/questions/QuestionApiController.java
@@ -0,0 +1,68 @@
+package com.nighthawk.spring_portfolio.mvc.questions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.*;
+
+@RestController // annotation to simplify the creation of RESTful web services
+@RequestMapping("/api/mcq")
+public class QuestionApiController {
+    // Autowired enables Control to connect HTML and POJO Object to database easily for CRUD operations
+    @Autowired
+    private QuestionJpa repository;
+
+    private String getUserName() {
+        // gets username from authentication, used to attribute objects to users
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return username;
+    }
+    
+    @GetMapping("/get/")
+    public ResponseEntity<List<QuestionObject>> getStatsObjects() {
+        // Get user's objects by userid
+        return new ResponseEntity<>(repository.findByowner(getUserName()), HttpStatus.OK);
+    }
+
+    // POST method to create object
+    @PostMapping("/create") 
+    public ResponseEntity<List<QuestionObject>> createStatsObject(@RequestParam("question") String question, @RequestParam("answer") String answer) {
+        // Create new object and save to repo
+        String username = getUserName();
+        QuestionObject a = new QuestionObject(question, answer);
+        repository.save(a);
+        return new ResponseEntity<>(repository.findByowner(username), HttpStatus.OK);
+    }
+    
+
+    // @GetMapping("/scrub/{objectID}")
+    // public ResponseEntity<QuestionObject> scrub(@PathVariable int objectID) {
+    //     QuestionObject a = repository.findById(objectID).get();
+    //     // Check if owner matches
+    //     if (!(a.getOwner().equals(getUserName()))) {
+    //         return new ResponseEntity<>(a, HttpStatus.BAD_REQUEST);
+    //     }
+    //     // Scrub object history and save to repo
+    //     a.clearHistory();
+    //     repository.save(a);
+    //     return new ResponseEntity<>(a, HttpStatus.OK);
+    // }
+
+    // @GetMapping("/delete/{objectID}")
+    // public ResponseEntity<List<QuestionObject>> delete(@PathVariable int objectID) {
+    //     QuestionObject a = repository.findById(objectID).get();
+    //     // Check if owner matches
+    //     if (!(a.getOwner().equals(getUserName()))) {
+    //         return new ResponseEntity<>(repository.findByowner(getUserName()), HttpStatus.BAD_REQUEST);
+    //     }
+    //     // Delete object from repo
+    //     repository.delete(a);
+    //     return new ResponseEntity<>(repository.findByowner(getUserName()), HttpStatus.OK);
+    // }
+}
