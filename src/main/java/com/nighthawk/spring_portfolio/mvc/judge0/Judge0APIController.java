38b60afcd30dadd255c57a8fee8ce0461b6a7a91
dtsivkovski <dtsivkovs@gmail.com>
2023-04-06 09:14:26 -0700

    potential fix to j0

--- src/main/java/com/nighthawk/spring_portfolio/mvc/judge0/Judge0APIController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/judge0/Judge0APIController.java
@@ -9,6 +9,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -16,8 +17,12 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.Base64;
+import java.util.Optional;
+
 
 @RestController
+@RequestMapping("/j0/")
 public class Judge0APIController {
     private static final String API_URL = "http://34.205.167.218:2358/";
 
@@ -49,13 +54,13 @@ public class Judge0APIController {
     private String pollSubmissionStatus(String submissionId) throws IOException {
         while (true) {
             HttpGet request = new HttpGet(API_URL + "submissions/" + submissionId + "?base64_encoded=true");
-
+    
             HttpResponse response = HttpClientBuilder.create().build().execute(request);
             String responseJson = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-
+    
             Map<String, Object> responseData = new Gson().fromJson(responseJson, HashMap.class);
             String status = responseData.get("status").toString();
-
+    
             if ("queued".equals(status) || "processing".equals(status)) {
                 try {
                     Thread.sleep(1000);
@@ -63,9 +68,31 @@ public class Judge0APIController {
                     e.printStackTrace();
                 }
             } else {
-                return new String(java.util.Base64.getDecoder().decode((String) responseData.get("stdout")),
-                        StandardCharsets.UTF_8);
+                String stdout = (String) responseData.get("stdout");
+                if (stdout != null && !stdout.isEmpty()) {
+                    Optional<Base64.Decoder> decoder = Optional.ofNullable(Base64.getDecoder());
+    
+                    byte[] decodedBytes = decoder.map(d -> d.decode(stdout)).orElse(new byte[0]);
+                    String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+                    return decodedString;
+                } else {
+                    return "";
+                }
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            // java atob
+            String str = "public class Main { public static void main(String[] args) { System.out.println(\"Hello World\"); } }";
+            String encoded = Base64.getEncoder().encodeToString(str.getBytes());
+
+            System.out.println(encoded);
+
+            System.out.println(new Judge0APIController().runCode(encoded));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
