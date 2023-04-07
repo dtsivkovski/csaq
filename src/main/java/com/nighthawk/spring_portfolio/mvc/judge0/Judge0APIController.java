2b9b58b8a5fe3c371c95191bb0f41bd0e1bae0ea
dtsivkovski <dtsivkovs@gmail.com>
2023-04-07 09:33:55 -0700

    among us

--- src/main/java/com/nighthawk/spring_portfolio/mvc/judge0/Judge0APIController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/judge0/Judge0APIController.java
@@ -1,18 +1,18 @@
 package com.nighthawk.spring_portfolio.mvc.judge0;
 
 import com.google.gson.Gson;
-import org.apache.http.HttpResponse;
+
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,28 +29,38 @@ public class Judge0APIController {
     @PostMapping("/run")
     public String runCode(@RequestBody String code) throws IOException {
         String submissionId = submitCode(code);
-        return pollSubmissionStatus(submissionId);
+        // return pollSubmissionStatus(submissionId);
+        return submissionId;
     }
 
     private String submitCode(String code) throws IOException {
-        Map<String, Object> requestData = new HashMap<>();
-        requestData.put("source_code", code);
-        requestData.put("language_id", 62); // Java language ID
-        requestData.put("stdin", "");
 
-        String requestJson = new Gson().toJson(requestData);
+        String requestJson = "{\"stdin\":\"\",\"language_id\":62,\"source_code\":\""+code+"\"}";
+        System.out.println("Request: dude " + requestJson);
 
-        HttpPost request = new HttpPost(API_URL + "submissions");
-        request.setHeader("Content-type", "application/json");
-        request.setEntity(new StringEntity(requestJson, StandardCharsets.UTF_8));
 
-        HttpResponse response = HttpClientBuilder.create().build().execute(request);
-        String responseJson = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(API_URL + "submissions/?base64_encoded=true"))
+            .header("content-type", "application/json")
+            .header("Content-Type", "application/json")
+            .method("POST", HttpRequest.BodyPublishers.ofString("{\r\n    \"language_id\": 52,\r\n    \"source_code\": \"I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBuYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxvLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0=\",\r\n    \"stdin\": \"SnVkZ2Uw\"\r\n}"))
+            .build();
+        
+            try {
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                
+                System.out.println(response.body());
+                return (String) response.body();
+            } catch (InterruptedException e) {
+                // Handle the InterruptedException exception here
+                return "Error";
+            }
 
-        Map<String, Object> responseData = new Gson().fromJson(responseJson, HashMap.class);
-        return (String) responseData.get("token");
+        // Map<String, Object> responseData = new Gson().fromJson(responseJson, HashMap.class);
+        // System.out.println(responseData.get("token"));
     }
 
+    /*
     private String pollSubmissionStatus(String submissionId) throws IOException {
         while (true) {
             HttpGet request = new HttpGet(API_URL + "submissions/" + submissionId + "?base64_encoded=true");
@@ -59,9 +69,15 @@ public class Judge0APIController {
             String responseJson = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
     
             Map<String, Object> responseData = new Gson().fromJson(responseJson, HashMap.class);
-            String status = responseData.get("status").toString();
+            System.out.println(responseData);
+            System.out.println("Bruhhhhhh" + responseData.getClass());
+            Map<String, Object> status = (Map<String, Object>) responseData.get("status");
+            String description = (String) status.get("description");
+
+            // String status = responseData.get("status").getAsJsonObject().get("description").getAsString();
+
     
-            if ("queued".equals(status) || "processing".equals(status)) {
+            if ("In Queue".equals(description) || "Processing".equals(description)) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -81,16 +97,15 @@ public class Judge0APIController {
             }
         }
     }
+    */
 
     public static void main(String[] args) {
         try {
             // java atob
-            String str = "public class Main { public static void main(String[] args) { System.out.println(\"Hello World\"); } }";
-            String encoded = Base64.getEncoder().encodeToString(str.getBytes());
+            String str = "cHJpbnQgY2xhc3MgTWFpbiB7IHBvcnRzIHN0YXRpYyBvZiBjb25zdHJ1Y3QgZXh0ZXJpbmcgZXhwb3J0czt9IAoKRG9jdW1lbnQgY29uc3QgbWFpbigpIHsKCgkJc3lzdGVtLmZyb20oJ2MnKTsKfQo=";
+            System.out.println(str);
 
-            System.out.println(encoded);
-
-            System.out.println(new Judge0APIController().runCode(encoded));
+            System.out.println(new Judge0APIController().submitCode(str));
         } catch (IOException e) {
             e.printStackTrace();
         }
