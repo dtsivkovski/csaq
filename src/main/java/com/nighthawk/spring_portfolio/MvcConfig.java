c26ff05cfba0b2380068698262cc599c4d6976fc
dtsivkovski <dtsivkovs@gmail.com>
2023-04-17 20:47:02 -0700

    try cors pls

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -21,10 +21,10 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
     }
 
-    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("https://nathanmanangan.github.io");
+        // "http://localhost:4000", "http://localhost:4001"
     }
-    
+
 }
